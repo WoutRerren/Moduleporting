c4ee8230f95d93c1777d89e7e1a35593b14b0079
Wout <wout.rerren.w1125@student.hogent.be>
2017-12-19 12:50:42 +0100

    buildform verplaatst naar de php file en aangepast naar de nieuwe vereiste

--- src/Form/ScrollToTopForm.php
+++ src/Form/ScrollToTopForm.php
@@ -18,4 +18,63 @@ class ScrollToTopForm extends ConfigFormBase {
   public function getFormId() {
     return 'scroll_to_top_form';
   }
+    
+  /**
+  * {@inheritdoc}
+  */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+
+    $config = $this->config('scroll_to_top.settings');
+
+    $form['scroll_to_top_label'] = array(
+      '#type' => 'textfield',
+      '#title' => $this->t('Label'),
+      '#description' => $this->t('label displayed in scroll to top link, default "Back to top".'),
+      '#default_value' => $this->t($config->get('scroll_to_top_label')),
+      '#size' => 10,
+    );
+    $form['scroll_to_top_position'] = array(
+      '#title' => $this->t( 'Position' ),
+      '#description' => $this->t('Sroll to top button position'),
+      '#type' => 'select',
+      '#options' => array(
+        1 => $this->t('right'),
+        2 => $this->t('left'),
+        3 => $this->t('middle'),
+      ),
+      '#default_value' => $config->get('scroll_to_top_position'),
+    );
+
+    $form['scroll_to_top_bg_color_hover'] = array(
+      '#type' => 'color',
+      '#title' => $this->t('Background color on mouse over.'),
+      '#description' => $this->t('Button background color on mouse over default #006595'),
+      '#default_value' => $config->get('scroll_to_top_bg_color_hover'),
+    );
+    $form['scroll_to_top_bg_color_out'] = array(
+      '#type' => 'color',
+      '#title' => $this->t('Background color on mouse out.'),
+      '#description' => $this->t('Button background color on mouse over default #CCCCCC'),
+      '#default_value' => $config->get('scroll_to_top_bg_color_out'),
+    );
+    $form['scroll_to_top_display_text'] = array(
+      '#type' => 'checkbox',
+      '#title' => $this->t('Display label'),
+      '#description' => $this->t('Display "BACK TO TOP" text under the button'),
+      '#default_value' => $config->get('scroll_to_top_display_text'),
+    );
+    $form['scroll_to_top_enable_admin_theme'] = array(
+      '#type' => 'checkbox',
+      '#title' => $this->t('Enable on administration theme.'),
+      '#description' => $this->t('Enable scroll to top button on administartion theme.'),
+      '#default_value' => $config->get('scroll_to_top_enable_admin_theme'),
+    );
+    $form['scroll_to_top_preview'] = array(
+      '#type' => 'item',
+      '#title' => $this->t('Preview'),
+      '#markup' => '<div id="scroll-to-top-prev-container">' . $this->t('Change a setting value to see a preview. "Position" and "enable on admin theme" not included.') . '</div>',
+    );
+
+    return parent::buildForm($form, $form_state);
+  }
 }
